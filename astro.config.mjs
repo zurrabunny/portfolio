2cb6fe4561b8496950611531d06535b7e1173d9d
tokkiperwinks <188626430+zurrabunny@users.noreply.github.com>
2025-11-26 20:50:36 +0800

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zurrabunny.github.io/portfolio/',
   base: '/portfolio/',
   integrations: [mdx()],
+  adapter: cloudflare(),
 });
