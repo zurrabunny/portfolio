c2a55f7abb06d75c09519797d83644790a82d4d1
tokkiperwinks <188626430+zurrabunny@users.noreply.github.com>
2025-11-26 20:52:49 +0800

    Revert "cloudflare"
    This reverts commit 2cb6fe4561b8496950611531d06535b7e1173d9d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zurrabunny.github.io/portfolio/',
   base: '/portfolio/',
   integrations: [mdx()],
-  adapter: cloudflare(),
 });
